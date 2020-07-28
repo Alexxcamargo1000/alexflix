d098a515807c30cb44bc88ae2c536a1d37815187
Alexxcamargo1000 <alexxcamargo1000@gmail.com>
2020-07-28 19:45:48 -0300

    melhorando o carrossel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { VideoCardGroupContainer, VideoCardList, Title, ExtraLink, Text} from './styles';
+import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
+import Slider, { SliderItem } from './components/Slider';
 
-function VideoCardGroup({
+function Carousel({
   ignoreFirstVideo,
   category,
 }) {
@@ -10,7 +11,6 @@ function VideoCardGroup({
   const categoryColor = category.cor;
   const categoryExtraLink = category.link_extra;
   const videos = category.videos;
-  const categoryText = category.text
   return (
     <VideoCardGroupContainer>
       {categoryTitle && (
@@ -18,34 +18,32 @@ function VideoCardGroup({
           <Title style={{ backgroundColor: categoryColor || 'red' }}>
             {categoryTitle}
           </Title>
-      <Text>{categoryText}</Text>
           {categoryExtraLink && 
             <ExtraLink href={categoryExtraLink.url} target="_blank">
               {categoryExtraLink.text}  
             </ExtraLink>
-          
           }
         </>
       )}
-      <VideoCardList>
+      <Slider>
         {videos.map((video, index) => {
           if (ignoreFirstVideo && index === 0) {
             return null;
           }
 
           return (
-            <li key={video.titulo}>
+            <SliderItem key={video.titulo}>
               <VideoCard
                 videoTitle={video.titulo}
                 videoURL={video.url}
                 categoryColor={categoryColor}
               />
-            </li>
+            </SliderItem>
           );
         })}
-      </VideoCardList>
+      </Slider>
     </VideoCardGroupContainer>
   );
 }
 
-export default VideoCardGroup;
+export default Carousel;
