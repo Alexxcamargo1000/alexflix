a5e59ee7f2b913868d50591bb611a8bad4c95a12
Alexxcamargo1000 <alexxcamargo1000@gmail.com>
2020-07-30 10:23:13 -0300

    random banner

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,8 +8,8 @@ const Random = function(max){
   return Math.floor(Math.random() * max)
 }
 
-const number = Random(dadosIniciais.categorias.length)
-const number2 = Random(dadosIniciais.categorias[number].videos.length)
+const randomCategoria = Random(dadosIniciais.categorias.length)
+const randomVideo = Random(dadosIniciais.categorias[randomCategoria].videos.length)
 function Home() {
   return (
 
@@ -21,15 +21,10 @@ function Home() {
         url={dadosIniciais.categorias[0].videos[0].url}
         videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
       /> */}
-       {/* <BannerMain
-        videoTitle={dadosIniciais.categorias[number].videos[number2].titulo}
-        url={dadosIniciais.categorias[number].videos[number2].url}
-        videoDescription={dadosIniciais.categorias[number].videos[number2].descricao}
-      /> */}
-      <BannerMain
-        videoTitle={dadosIniciais.categorias[4].videos[6].titulo}
-        url={dadosIniciais.categorias[4].videos[6].url}
-        videoDescription={dadosIniciais.categorias[4].videos[6].descricao}
+       <BannerMain
+        videoTitle={dadosIniciais.categorias[randomCategoria].videos[randomVideo].titulo}
+        url={dadosIniciais.categorias[randomCategoria].videos[randomVideo].url}
+        videoDescription={dadosIniciais.categorias[randomCategoria].videos[randomVideo].descricao}
       />
 
       <Carousel
